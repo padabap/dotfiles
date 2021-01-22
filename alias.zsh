9174f6e23c493dbeb704e07be5d94e126946d01b
Joyce Woo <yyjoycewoo@gmail.com>
2021-01-22 09:54:12 -0700

    Add alias.zsh

---
+++ alias.zsh
@@ -0,0 +1,15 @@
+# For a full list of active aliases, run `alias`.
+
+# ls
+alias ll='ls -l'
+alias la='ls -a'
+
+# cd
+alias ~='cd ~'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+# paths
+alias go.h="~/"
+alias go.d="~/Desktop"
